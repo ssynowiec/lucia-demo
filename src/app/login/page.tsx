0b867614a57824a294c146ff75a612dbfb76262b
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-17 20:26:34 +0100

    add: link to register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,14 @@
 import { LoginForm } from '@/components/ui/login-form';
+import Link from 'next/link';
 
 const Login = () => {
   return (
     <div className='flex w-full flex-col items-center justify-center'>
       <h1>Login</h1>
       <LoginForm />
+      <Link href='/register' className='mt-4'>
+        You do not have an account?
+      </Link>
     </div>
   );
 };
