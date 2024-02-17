8da4abdd4328f205126921ee944e494183f6a5f3
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-17 19:37:15 +0100

    add: login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,12 @@
+import { LoginForm } from '@/components/ui/login-form';
+
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className='flex w-full flex-col items-center justify-center'>
+      <h1>Login</h1>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/ui/login-form.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const loginFormSchema = z.object({
+  login: z.string(),
+  password: z.string(),
+});
+
+export const LoginForm = () => {
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof loginFormSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name='login'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Login</FormLabel>
+              <FormControl>
+                <Input placeholder='johndoe' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder='**********' {...field} type='password' />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Login</Button>
+      </form>
+    </Form>
+  );
+};
