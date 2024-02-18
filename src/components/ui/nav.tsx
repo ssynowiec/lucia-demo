62d54ce88f335a6f41d0e2385c4ebaf0d362c386
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-18 19:29:03 +0100

    add: protected page & logout to meny

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -9,27 +9,45 @@ import {
 import Link from 'next/link';
 import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 import { ModeToggle } from '@/components/ui/theme-switcher';
+import { Button } from '@/components/ui/button';
+import { logout } from '@/auth/logout';
 
 export const Nav = () => {
   return (
     <NavigationMenu className='mx-auto flex h-10 w-full max-w-screen-lg justify-between'>
-      <NavigationMenuList>
+      <NavigationMenuList className='flex'>
         <NavigationMenuItem>
           <Link href='/' legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Home
             </NavigationMenuLink>
           </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
           <Link href='/login' legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Login
             </NavigationMenuLink>
           </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
           <Link href='/register' legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Register
             </NavigationMenuLink>
           </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href='/protected' legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Protected
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <form action={logout}>
+            <Button variant='link'>Logout</Button>
+          </form>
         </NavigationMenuItem>
       </NavigationMenuList>
       <ModeToggle />
