1158313ef4c2d30bdf7e30da9b18000b83db7aa3
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-18 19:37:49 +0100

    add: button login with github

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
 import { LoginForm } from '@/components/ui/login-form';
 import Link from 'next/link';
 import type { Metadata } from 'next';
+import { Button } from '@/components/ui/button';
+import { Github } from 'lucide-react';
 
 export const metadata: Metadata = {
   title: 'Login',
@@ -12,6 +14,11 @@ const Login = () => {
     <div className='flex w-full flex-col items-center justify-center'>
       <h1 className='text-6xl font-extrabold'>Login</h1>
       <LoginForm />
+      <Button variant='outline' asChild={true} className='mt-4'>
+        <Link href='/login/github'>
+          <Github className='mr-2' /> Login with Github
+        </Link>
+      </Button>
       <Link href='/register' className='mt-4'>
         You do not have an account?
       </Link>
