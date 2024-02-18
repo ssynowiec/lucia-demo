e3da3d61cb8d00d40c53c0f13dfe9eb82f6469ea
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-18 18:50:16 +0100

    style: h1 font & add metadata

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,16 @@
 import { LoginForm } from '@/components/ui/login-form';
 import Link from 'next/link';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Login to your account.',
+};
 
 const Login = () => {
   return (
     <div className='flex w-full flex-col items-center justify-center'>
-      <h1>Login</h1>
+      <h1 className='text-6xl font-extrabold'>Login</h1>
       <LoginForm />
       <Link href='/register' className='mt-4'>
         You do not have an account?
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,15 @@
 import { RegisterForm } from '@/components/ui/register-form';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Register',
+  description: 'Create your account.',
+};
 
 const Register = () => {
   return (
     <div className='flex w-full flex-col items-center justify-center'>
-      <h1>Create an account</h1>
+      <h1 className='text-6xl font-extrabold'>Create an account</h1>
       <RegisterForm />
     </div>
   );
