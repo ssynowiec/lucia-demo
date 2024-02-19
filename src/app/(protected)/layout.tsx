c62d6ef8f7485b4994e684b0857325f0976d8473
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-19 10:04:14 +0100

    fix: uncomment protect layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,15 +1,17 @@
 import type { ReactNode } from 'react';
+import { validateRequest } from '@/auth/validate-request';
+import { redirect } from 'next/navigation';
 
 interface ProtectedLayoutProps {
   children: ReactNode;
 }
 
 const ProtectedLayout = async ({ children }: ProtectedLayoutProps) => {
-  // const { user } = await validateRequest();
-  //
-  // if (!user) {
-  //   return redirect('/login');
-  // }
+  const { user } = await validateRequest();
+
+  if (!user) {
+    return redirect('/login');
+  }
 
   return <>{children}</>;
 };
