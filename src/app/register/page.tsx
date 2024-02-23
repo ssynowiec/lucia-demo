039674c4e41ab42ddd49beccca99873a354903e2
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-23 13:10:46 +0100

    add: redirect from register page if user logged

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,20 @@
 import { RegisterForm } from '@/components/ui/register-form';
 import type { Metadata } from 'next';
+import { validateRequest } from '@/auth/validate-request';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Register | Lucia auth demo',
   description: 'Create your account.',
 };
 
-const Register = () => {
+const Register = async () => {
+  const { user } = await validateRequest();
+
+  if (user) {
+    return redirect('/protected');
+  }
+
   return (
     <div className='flex w-full flex-col items-center justify-center'>
       <h1 className='text-6xl font-extrabold'>Create an account</h1>
